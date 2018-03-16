d22f405e28e192567dd76b890417bd7751ca2c12
qus1225 <qus1225@gmail.com>
2018-03-16 18:36:19 +0900

    Render Form with WTF

--- hello.py
+++ hello.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', form=NameForm())
 
 @app.route('/user/<name>')
 def user(name):
