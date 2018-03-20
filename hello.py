462d32e9c34139bde6f6f40f2a8e276f5ffa0564
qus1225 <qus1225@gmail.com>
2018-03-20 10:47:56 +0900

    Flash message

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
@@ -21,9 +21,14 @@ bootstrap = Bootstrap(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
