12b264f2935a32b832b59c9b7be60f4446049f16
qus1225 <qus1225@gmail.com>
2018-03-23 18:08:04 +0900

    Debug - Correct Typo

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-migrate = Manager(app, db)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
