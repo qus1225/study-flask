f49b473f58d5cf58ba0ab49b0dcb3eb429413700
qus1225 <qus1225@gmail.com>
2018-03-29 15:21:14 +0900

    Add Blueprint - auth

--- app/__init__.py
+++ app/__init__.py
@@ -22,5 +22,7 @@ def create_app(config_name):
     # To avoid circular dependency, Locate blueprint below
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
